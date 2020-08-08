2de1643d7c583c03bec9477c29a50070e4dd9e27
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-08 18:40:11 -0300

    added route to ForgotPassword

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -82,7 +82,7 @@ const SignIn: React.FC = () => {
 
             <Button type="submit">Entrar</Button>
 
-            <Link to="forgot">Esqueci minha senha</Link>
+            <Link to="/forgot-password">Esqueci minha senha</Link>
           </Form>
 
           <Link to="signup">

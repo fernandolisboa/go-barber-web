83d25c8575ecc8fb34ae1a18ff8199bb05f2bbc8
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-09 00:49:18 -0300

    init Schedule > NextAppointment with dummies (html & css)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
@@ -29,6 +38,38 @@ const Dashboard: React.FC = () => {
             <FiPower />
           </button>
         </HeaderContent>
+
+        <Content>
+          <Schedule>
+            <h1>Horários agendados</h1>
+
+            <p>
+              <span>Hoje</span>
+              <span>Dia 06</span>
+              <span>Segunda-feira</span>
+            </p>
+
+            <NextAppointment>
+              <strong>Próximo atendimento</strong>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/6035851?s=460&u=eb0ef7936a77bdddc04ec3a818ba74164037a8a0&v=4"
+                  alt="Fernando Lisboa"
+                />
+
+                <strong>Fernando Lisboa</strong>
+
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+              </div>
+            </NextAppointment>
+          </Schedule>
+
+          <Calendar />
+        </Content>
       </Header>
     </Container>
   );

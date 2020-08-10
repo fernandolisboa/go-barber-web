7ab1dddcf62a3dc2c4dae94ecb24471f13cca0df
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-10 00:27:01 -0300

    getting month-availability & scheduled appointments from the api (+ separating into morning & afternoon appointments)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import { FiPower, FiClock } from 'react-icons/fi';
 
+import { isToday, format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
@@ -28,6 +31,16 @@ interface IMonthAvailabilityItem {
   available: boolean;
 }
 
+interface IAppointment {
+  id: string;
+  date: string;
+  formattedHour: string;
+  customer: {
+    name: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
 
@@ -36,6 +49,7 @@ const Dashboard: React.FC = () => {
   const [monthAvailability, setMonthAvailability] = useState<
     IMonthAvailabilityItem[]
   >([]);
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
 
   const handleDateChange = useCallback(
     (day: Date, modifiers: DayModifiers) =>
@@ -50,12 +64,15 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     api
-      .get(`/providers/${user.id}/month-availability`, {
-        params: {
-          month: currentMonth.getMonth() + 1,
-          year: currentMonth.getFullYear(),
+      .get<IMonthAvailabilityItem[]>(
+        `/providers/${user.id}/month-availability`,
+        {
+          params: {
+            month: currentMonth.getMonth() + 1,
+            year: currentMonth.getFullYear(),
+          },
         },
-      })
+      )
       .then(({ data }) => setMonthAvailability(data));
   }, [currentMonth, user.id]);
 
@@ -71,6 +88,45 @@ const Dashboard: React.FC = () => {
 
     return disabledDates;
   }, [currentMonth, monthAvailability]);
+
+  const selectedDayAsText = useMemo(
+    () => format(selectedDate, "'Dia' dd 'de' MMMM", { locale: ptBR }),
+    [selectedDate],
+  );
+
+  const selectedWeekDay = useMemo(
+    () => format(selectedDate, 'cccc', { locale: ptBR }),
+    [selectedDate],
+  );
+
+  useEffect(() => {
+    api
+      .get<IAppointment[]>('/appointments/me', {
+        params: {
+          day: selectedDate.getDate(),
+          month: selectedDate.getMonth() + 1,
+          year: selectedDate.getFullYear(),
+        },
+      })
+      .then(({ data }) => {
+        const formattedAppointments = data.map(appointment => {
+          return {
+            ...appointment,
+            formattedHour: format(parseISO(appointment.date), 'HH:mm'),
+          };
+        });
+
+        setAppointments(formattedAppointments);
+      });
+  }, [selectedDate]);
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter(({ date }) => parseISO(date).getHours() < 12);
+  }, [appointments]);
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter(({ date }) => parseISO(date).getHours() >= 2);
+  }, [appointments]);
 
   return (
     <Container>
@@ -98,9 +154,9 @@ const Dashboard: React.FC = () => {
           <h1>Horários agendados</h1>
 
           <p>
-            <span>Hoje</span>
-            <span>Dia 06</span>
-            <span>Segunda-feira</span>
+            {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDayAsText}</span>
+            <span>{selectedWeekDay}</span>
           </p>
 
           <NextAppointment>
@@ -124,73 +180,43 @@ const Dashboard: React.FC = () => {
           <Section>
             <strong>Manhã</strong>
 
-            <Appointment>
-              <span>
-                <FiClock />
-                08:00
-              </span>
+            {morningAppointments.map(
+              ({ id, formattedHour, customer: { name, avatar_url } }) => (
+                <Appointment key={id}>
+                  <span>
+                    <FiClock />
+                    {formattedHour}
+                  </span>
 
-              <div>
-                <img
-                  src="https://avatars2.githubusercontent.com/u/6035851?s=460&u=eb0ef7936a77bdddc04ec3a818ba74164037a8a0&v=4"
-                  alt="Fernando Lisboa"
-                />
+                  <div>
+                    <img src={avatar_url} alt={name} />
 
-                <strong>Fernando Lisboa</strong>
-              </div>
-            </Appointment>
-
-            <Appointment>
-              <span>
-                <FiClock />
-                10:00
-              </span>
-
-              <div>
-                <img
-                  src="https://avatars2.githubusercontent.com/u/6035851?s=460&u=eb0ef7936a77bdddc04ec3a818ba74164037a8a0&v=4"
-                  alt="Macaco Albino"
-                />
-
-                <strong>Macaco Albino</strong>
-              </div>
-            </Appointment>
+                    <strong>{name}</strong>
+                  </div>
+                </Appointment>
+              ),
+            )}
           </Section>
 
           <Section>
             <strong>Tarde</strong>
 
-            <Appointment>
-              <span>
-                <FiClock />
-                11:00
-              </span>
+            {afternoonAppointments.map(
+              ({ id, formattedHour, customer: { name, avatar_url } }) => (
+                <Appointment key={id}>
+                  <span>
+                    <FiClock />
+                    {formattedHour}
+                  </span>
 
-              <div>
-                <img
-                  src="https://avatars2.githubusercontent.com/u/6035851?s=460&u=eb0ef7936a77bdddc04ec3a818ba74164037a8a0&v=4"
-                  alt="Luiz Gabriel"
-                />
+                  <div>
+                    <img src={avatar_url} alt={name} />
 
-                <strong>Luiz Gabriel</strong>
-              </div>
-            </Appointment>
-
-            <Appointment>
-              <span>
-                <FiClock />
-                15:00
-              </span>
-
-              <div>
-                <img
-                  src="https://avatars2.githubusercontent.com/u/6035851?s=460&u=eb0ef7936a77bdddc04ec3a818ba74164037a8a0&v=4"
-                  alt="Pedro Miguel"
-                />
-
-                <strong>Pedro Miguel</strong>
-              </div>
-            </Appointment>
+                    <strong>{name}</strong>
+                  </div>
+                </Appointment>
+              ),
+            )}
           </Section>
         </Schedule>
         <Calendar>
